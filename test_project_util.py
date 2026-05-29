214d85945560dc25160651f30b965b8cc1a4fa49
Lambert Nguyen <lambert@Lamberts-MacBook-Pro.local>
2026-05-29 15:13:21 -0700

    Initial commit

---
+++ test_project_util.py
@@ -0,0 +1,4 @@
+# Test Project Utility Functions
+
+## This file is currently in progress. 
+## The goal of this will be to test the project_util functions by checking for errors at the expected bounds. This will also check that the expected values are reported as expected (zero all but one parameter)
